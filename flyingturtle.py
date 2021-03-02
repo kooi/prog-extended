d4cf998ef7213155551eebdefd7c6e6edc6728c1
Mark Kooijman <kooi.barlaeus@gmail.com>
2021-03-02 09:17:15 +0100

    jumping limit

--- flyingturtle.py
+++ flyingturtle.py
@@ -5,17 +5,16 @@ class FlyingTurtle(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("turtle")
-        self.shapesize(3)
         self.vy = 0
         self.vx = 5
         self.ay = -1
-        self.bottom = 0
+        self.bottom = -200
         self.right = 200
         self.left = -200
         self.move()
 
     def move(self):
-        nx = self.xcor() + self.vx
+        nx = self.xcor() + self.vx 
         ny = self.ycor() + self.vy
         
         if ny < self.bottom:
@@ -30,10 +29,16 @@ class FlyingTurtle(turtle.Turtle):
         self.vy = self.vy + self.ay
         
     def jump(self):
-        self.vy = self.vy + 10
+        if self.ycor() == self.bottom:
+            self.vy = self.vy + 10
+    
+    def run(self):
+        self.vx = self.vx + 1
 
 tina = FlyingTurtle()
 
 tina.getscreen().onkey(tina.jump, "space")
+tina.getscreen().onkey(tina.run, "Right")
 tina.getscreen().listen()
+
 
